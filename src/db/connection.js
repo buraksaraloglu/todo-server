6677d4978b43e165881ea455bf5cd72c817ac1f7
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-18 00:26:02 +0300

    bug fix on mongodb connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,18 +4,16 @@ require('dotenv').config();
 
 const URI = process.env.DB_URI;
 
-const connectDB = async () => {
+const connectDB = () => {
   URI &&
-    (await mongoose
+    mongoose
       .connect(URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
       })
-      // eslint-disable-next-line no-console
-      .then(() => {}))
-      // eslint-disable-next-line no-console
-      .catch((error) => console.log(error)));
+      .then(() => {})
+      .catch((error) => console.log(error));
 };
 
 module.exports = connectDB;

2582cf2ca18f16a51ea3fcc2292a0c0a61de0585
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-18 00:15:03 +0300

    mongodb url fix

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,17 +4,18 @@ require('dotenv').config();
 
 const URI = process.env.DB_URI;
 
-const connectDB = () => {
-  mongoose
-    .connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-    // eslint-disable-next-line no-console
-    .then(console.log('db connected'))
-    // eslint-disable-next-line no-console
-    .catch((error) => console.log(error));
+const connectDB = async () => {
+  URI &&
+    (await mongoose
+      .connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      })
+      // eslint-disable-next-line no-console
+      .then(console.log('db connected'))
+      // eslint-disable-next-line no-console
+      .catch((error) => console.log(error)));
 };
 
 module.exports = connectDB;
